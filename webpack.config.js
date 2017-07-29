cd95af164671aa7faf38e926d817aaf4316b4277
James Piskorz <james@omnomnomstudios.com>
2017-07-29 17:36:29 +1000

    WIP deployment issues

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel'],
+      loaders: ['babel-loader'],
       include: __dirname,
       exclude: /node_modules/,
     }]
